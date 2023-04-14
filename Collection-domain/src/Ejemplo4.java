0884a754609c489def3d495944bd888ca8c60afd
Avila <avila1996jose@gmail.com>
2023-04-14 01:01:03 -0500

    trabajando con array de objetos sobreescribiendo toString

---
+++ Collection-domain/src/Ejemplo4.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Ejemplo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Curso curso1 =new Curso( "Geometria", 30);
+		 Curso curso2 =new Curso( "Fisica", 30);
+		 Curso curso3 =new Curso( "Quimica", 30);
+		 Curso curso4 = new Curso( "History", 30);
+		 ArrayList<Curso> cursos = new ArrayList<>();
+		 
+		 cursos.add(curso1);
+		 cursos.add(curso2);
+		 cursos.add(curso3);
+		 cursos.add(curso4);
+		 
+		 System.out.println(cursos);
+		 
+	
+		 
+		
+	}
+
+}
