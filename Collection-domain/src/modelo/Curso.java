e13e462cfeec1847f3d88e720873dc31cc663976
Avila <avila1996jose@gmail.com>
2023-04-18 08:34:01 -0500

    modificando constructor con claselist

--- Collection-domain/src/modelo/Curso.java
+++ Collection-domain/src/modelo/Curso.java
@@ -1,9 +1,26 @@
 package modelo;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
 public class Curso {
 	private String nombre;
 	private int tiempo;
+	private List<Clase>claseList = new ArrayList<>();
+	
+	public Curso(String nombre, int tiempo) {
+		super();
+		this.nombre = nombre;
+		this.tiempo = tiempo;
+	}
+	public Curso(String nombre, int tiempo, List<Clase>claselist) {
+		super();
+		this.nombre = nombre;
+		this.tiempo = tiempo;
+		this.claseList = claselist;
+	}
+	
+	
 	public String getNombre() {
 		return nombre;
 	}
@@ -14,11 +31,6 @@ public class Curso {
 		return tiempo;
 	}
 	public void setTiempo(int tiempo) {
-		this.tiempo = tiempo;
-	}
-	public Curso(String nombre, int tiempo) {
-		super();
-		this.nombre = nombre;
 		this.tiempo = tiempo;
 	}
 	
