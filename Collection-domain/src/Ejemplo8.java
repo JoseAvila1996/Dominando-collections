9a5e737e78e05b34ebde43081fd0321f30e0ce12
Avila <avila1996jose@gmail.com>
2023-04-18 12:47:14 -0500

    agregando ejemplo8 diferencia de arraylist y linkedlist

---
+++ Collection-domain/src/Ejemplo8.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.stream.Collectors;
+
+import modelo.Aula;
+import modelo.Curso;
+
+public class Ejemplo8 {
+
+		final List<List<Integer>> listas = new ArrayList<List<Integer>>() {
+			{
+				add(new LinkedList<Integer>());
+				add(new ArrayList<Integer>());
+				
+			}
+		};
+		public static void main(String[] args) {
+	}
+
+}
